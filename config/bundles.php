ea8449a5cce201f57325a204d86000d7f7be6357
hamzael69 <elomarihamza835@gmail.com>
2025-06-18 14:28:24 +0200

    test vercel

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Helper\Bundle\DoctrineHelperBundle::class => ['all' => true],
+    Nakato53\Vercel\VercelBundle::class => ['dev' => true, 'test' => true],
 ];
